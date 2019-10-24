f49fec74008704cd1c85963dbfad037d3000f9c7
liaoyuankai <savent_gate@outlook.com>
2019-10-24 11:33:14 +0800

    first commit

---
+++ proj1.sql
@@ -0,0 +1,64 @@
+-- comp9311 19T3 Project 1
+--
+-- MyMyUNSW Solutions
+
+
+-- Q1:
+create or replace view Q1(unswid, longname)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q2:
+create or replace view Q2(unswid,name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q3:
+create or replace view Q3(unswid, name)
+as 
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q4:
+create or replace view Q4(num_student)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+--Q5:
+create or replace view Q5(code, name, semester)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q6:
+create or replace view Q6(num)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q7:
+create or replace view Q7(year, term, average_mark)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q8: 
+create or replace view Q8(zid, name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q9:
+create or replace view Q9(unswid, name)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
+
+-- Q10:
+create or replace view Q10(unswid, longname, num, rank)
+as
+--... SQL statements, possibly using other views/functions defined by you ...
+;
