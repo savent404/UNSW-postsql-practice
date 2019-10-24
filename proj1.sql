a8c1b87ff8694b4c6aa182b6727db2eea71e9b43
liaoyuankai <savent_gate@outlook.com>
2019-10-24 17:25:10 +0800

    ask for Q1&Q2

--- proj1.sql
+++ proj1.sql
@@ -5,23 +5,41 @@
 
 -- Q1:
 create or replace view Q1(unswid, longname)
-as
---... SQL statements, possibly using other views/functions defined by you ...
+as 
+	select r.unswid,r.longname 
+	from Rooms r join room_facilities f 
+	on (r.id=f.room and f.facility=19)
 ;
 
 -- Q2:
 create or replace view Q2(unswid,name)
-as
---... SQL statements, possibly using other views/functions defined by you ...
+as 
+	select distinct p.unswid,p.name 
+	from People p, staff s, course_enrolments c0, course_staff c1 
+	where (c0.course=c1.course and c1.staff=s.id and p.id=s.id and c0.student=(
+		select p.id 
+		from people p, students s 
+		where s.id=p.id and p.name='Hemma Margareta'
+	))
 ;
 
 -- Q3:
-create or replace view Q3(unswid, name)
+create or replace view tmp(unswid, name)
 as 
---... SQL statements, possibly using other views/functions defined by you ...
+	select distinct p.unswid,p.name, cc.mark
+	from subjects s, courses c, course_enrolments cc, people p 
+	where ((s.code='COMP9311' or s.code='COMP9024') and c.subject=s.id and cc.course=c.id and p.id=cc.student)
 ;
 
+create or replace view Q3(unswid, name)
+as 
+	select p.unswid,p.name  
+	from subjects s, courses c, course_enrolments cc, people p 
+	where ((s.code='COMP9311' or s.code='COMP9024') and c.subject=s.id and cc.course=c.id and p.id=cc.student and cc.mark > 84) 
+	group by p.unswid,p.name having count(p.name) > 1
+;
 -- Q4:
+
 create or replace view Q4(num_student)
 as
 --... SQL statements, possibly using other views/functions defined by you ...
